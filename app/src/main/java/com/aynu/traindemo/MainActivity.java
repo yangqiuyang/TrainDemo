15f65f451169d1f85952c8e528aa3184b7b194a2
yangqiuyang <898297475@qq.com>
2016-12-04 21:02:53 +0800

    1111
    111111111

--- app/src/main/java/com/aynu/traindemo/MainActivity.java
+++ app/src/main/java/com/aynu/traindemo/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         x.view().inject(this);
         initActionBar();
         initListenter();
+        //修改提交
         initView();
     }
     CompanyAdapter adapter;
