b61962e78a795e0f09b3431b89f4b244c067a5ef
yangqiuyang <898297475@qq.com>
2016-12-04 22:03:08 +0800

    克隆后的首次提交

--- app/src/main/java/com/aynu/applications/BaseActivity.java
+++ app/src/main/java/com/aynu/applications/BaseActivity.java
@@ -15,5 +15,5 @@ public class BaseActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
     }
-    /////////
+    /////////克隆后的
 }
