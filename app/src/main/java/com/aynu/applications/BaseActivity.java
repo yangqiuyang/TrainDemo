37e948b565b0b1eff6e90f3f5a592ab7e195ab2f
yangqiuyang <898297475@qq.com>
2016-12-04 21:41:04 +0800

    提交

---
+++ app/src/main/java/com/aynu/applications/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.aynu.applications;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/12/4.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
